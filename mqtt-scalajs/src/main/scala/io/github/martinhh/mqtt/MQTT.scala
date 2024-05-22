ea86126bd8aaa0ad56789da22d6cc222f85df21e
Martin Hansen <code@martin-hansen.de>
2024-05-22 18:47:09 +0200

    chore: add TODOs

--- mqtt-scalajs/src/main/scala/io/github/martinhh/mqtt/MQTT.scala
+++ mqtt-scalajs/src/main/scala/io/github/martinhh/mqtt/MQTT.scala
@@ -1,8 +1,5 @@
 package io.github.martinhh.mqtt
 
-import io.github.martinhh.mqtt.ClientOptions
-import io.github.martinhh.mqtt.MqttClient
-
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSImport
 
@@ -10,12 +7,24 @@ import scala.scalajs.js.annotation.JSImport
 @JSImport("mqtt", JSImport.Default)
 private object MQTT extends js.Object {
 
-  def connect(brokerUrl: String): MqttClient = js.native
-  def connect(brokerUrl: String, opts: ClientOptions): MqttClient = js.native
+  def connect(brokerUrl: String, opts: js.UndefOr[ClientOptions] = js.undefined): MqttClient =
+    js.native
+
+  def connectAsync(brokerUrl: String, opts: js.UndefOr[ClientOptions] = js.undefined): js.Promise[MqttClient] =
+    js.native
 }
 
-inline def connect(brokerUrl: String): MqttClient =
-  MQTT.connect(brokerUrl)
-
-inline def connect(brokerUrl: String, opts: ClientOptions): MqttClient =
+inline def connect(brokerUrl: String, opts: js.UndefOr[ClientOptions] = js.undefined): MqttClient =
   MQTT.connect(brokerUrl, opts)
+
+// TODO:
+//inline def connect(opts: ClientOptions): MqttClient =
+//    MQTT.connect(opts)
+
+
+inline def connectAsync(brokerUrl: String, opts: js.UndefOr[ClientOptions] = js.undefined): js.Promise[MqttClient] =
+  MQTT.connectAsync(brokerUrl, opts)
+
+// TODO:
+//inline def connectAsync(opts: ClientOptions): MqttClient =
+//    MQTT.connectAsync(opts)
