3b071dce69cafb9b61cf7e673b7ed3b939c780e7
Martin Hansen <code@martin-hansen.de>
2024-06-08 15:19:21 +0200

    refactor: make options mutable
    Make default implementations of various options-traits
    mutable. We cannot be sure which fields mqtt.js expects
    to be mutable, so we make them all vars (and hide those
    behind a defs-only base trait).

--- mqtt-scalajs/src/main/scala/io/github/martinhh/mqtt/ClientPublishOptions.scala
+++ mqtt-scalajs/src/main/scala/io/github/martinhh/mqtt/ClientPublishOptions.scala
@@ -7,16 +7,16 @@ import scala.scalajs.js
 /**
  * Corresponds to mqtt-js `IClientPublishOptions`
  */
-trait ClientPublishOptions extends js.Object {
-  def cbStorePut: js.UndefOr[StorePutCallback] = js.undefined
+sealed trait ClientPublishOptions extends js.Object {
+  def cbStorePut: js.UndefOr[StorePutCallback]
 
-  def dup: js.UndefOr[Boolean] = js.undefined
+  def dup: js.UndefOr[Boolean]
 
-  def properties: js.UndefOr[Mqtt5Properties] = js.undefined
+  def properties: js.UndefOr[Mqtt5Properties]
 
-  def qos: js.UndefOr[QoS] = js.undefined
+  def qos: js.UndefOr[QoS]
 
-  def retain: js.UndefOr[Boolean] = js.undefined
+  def retain: js.UndefOr[Boolean]
 }
 
 object ClientPublishOptions {
@@ -27,15 +27,26 @@ object ClientPublishOptions {
     properties: js.UndefOr[Mqtt5Properties] = js.undefined,
     qos: js.UndefOr[QoS] = js.undefined,
     retain: js.UndefOr[Boolean] = js.undefined
-  ): ClientPublishOptions =
-    ImmutableClientPublishOptions(cbStorePut, dup, properties, qos, retain)
+  ): ClientPublishOptions = {
+    val opts = new MutableClientPublishOptions {}
+    cbStorePut.foreach(_ => opts.cbStorePut = cbStorePut)
+    dup.foreach(_ => opts.dup = dup)
+    properties.foreach(_ => opts.properties = properties)
+    qos.foreach(_ => opts.qos = qos)
+    retain.foreach(_ => opts.retain = retain)
+    opts
+  }
 
-  private class ImmutableClientPublishOptions(
-    override val cbStorePut: js.UndefOr[StorePutCallback],
-    override val dup: js.UndefOr[Boolean],
-    override val properties: js.UndefOr[Mqtt5Properties],
-    override val qos: js.UndefOr[QoS],
-    override val retain: js.UndefOr[Boolean]
-  ) extends ClientPublishOptions
+}
 
+trait MutableClientPublishOptions extends ClientPublishOptions {
+  var cbStorePut: js.UndefOr[StorePutCallback] = js.undefined
+
+  var dup: js.UndefOr[Boolean] = js.undefined
+
+  var properties: js.UndefOr[Mqtt5Properties] = js.undefined
+
+  var qos: js.UndefOr[QoS] = js.undefined
+
+  var retain: js.UndefOr[Boolean] = js.undefined
 }
